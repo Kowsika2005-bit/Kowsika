221921369f312d45418b72ee0babf7e0d4acb9bc
Kowsika M <kkowsika07@gmail.com>
2025-05-16 15:57:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var a=10;
+var b=20;
+var c=a+b;
+console.log(a)
+console.log(b)
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(b/a)
+console.log(a>b)
+console.log(a==b)
+console.log(a<b)
+
+function add(){
+    var a=10;
+    var b=20;
+    console.log(a+b)
+}
+add();
+function add(a,b){
+    console.log(a+b)
+}
+add(10,40)
+add(5,5)
+function sub(a,b){
+    console.log(a-b)
+}
+sub(5,5)
